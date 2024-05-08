a579f61427c713452a1fb73fbb88e1e4eb61e329
subham351 <153291603+subham351@users.noreply.github.com>
2024-05-08 09:42:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os, re
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5975117303:AAFWqpfscM53-zT9wQxWVs5VqSud7wbcdTM")
 API_ID = int(os.environ.get("API_ID", "29329572"))
 API_HASH = os.environ.get("API_HASH", "b5d7c5d840ce6cd794c1754af5276bac")
-OWNER = int(os.environ.get("OWNER", ""))
+OWNER = int(os.environ.get("OWNER", "1956616671"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "ssp_spbot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
